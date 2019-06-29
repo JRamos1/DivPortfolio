0f46c7110610c482253430ea55768006f85817fc
Justin Ramos <jmramos396@gmail.com>
2019-06-29 10:00:32 -0400

    added concurrently to package.json under dependencies

---
+++ client/build/precache-manifest.d847a010631965504882ae74b318ff6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6987013001432858c07e",
+    "url": "/static/js/main.947c2441.chunk.js"
+  },
+  {
+    "revision": "c28573dde66f065768fe",
+    "url": "/static/js/2.ed878437.chunk.js"
+  },
+  {
+    "revision": "6987013001432858c07e",
+    "url": "/static/css/main.1508fc48.chunk.css"
+  },
+  {
+    "revision": "660f68ae88992f8921bd2aa7faf59bd2",
+    "url": "/index.html"
+  }
+];
